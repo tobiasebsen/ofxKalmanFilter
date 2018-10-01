ad86f26d224e82fc7f1fbb1c661fd2f753418687
Tobias Ebsen <tobiasebsen@gmail.com>
2018-10-01 09:25:33 +0200

    More intelligent label handelling

--- src/ofxKalmanFilter.cpp
+++ src/ofxKalmanFilter.cpp
@@ -9,24 +9,59 @@
 
 #define DEAD_LABEL 0xFFFFFFFF
 
-void ofxKalmanFilter::init(size_t elementSize, size_t count) {
+void ofxKalmanFilter::init(unsigned int elementSize, unsigned int count) {
     this->elementSize = elementSize;
     aliveLabels.resize(count, DEAD_LABEL);
+	numAlive = 0;
     KalmanFilter::init(count*elementSize);
 }
 
 void ofxKalmanFilter::keepLabels(const std::vector<unsigned int> &labels) {
-    for (int i=0; i<aliveLabels.size(); i++) {
+    numAlive = 0;
+	for (int i=0; i<aliveLabels.size(); i++) {
         bool found = false;
         for (unsigned int label : labels) {
             if (label == aliveLabels[i]) {
                 found = true;
+				numAlive++;
                 break;
             }
         }
         if (!found)
             aliveLabels[i] = DEAD_LABEL;
     }
+}
+
+unsigned int ofxKalmanFilter::getNumAlive() {
+	return numAlive;
+}
+
+unsigned int ofxKalmanFilter::getAliveLabel(unsigned int index) {
+	int j=0;
+	for (int i=0; i<aliveLabels.size(); i++) {
+		if (aliveLabels[i] != DEAD_LABEL) {
+			if (j == index)
+				return aliveLabels[i];
+			j++;
+		}
+	}
+	return DEAD_LABEL;
+}
+
+bool ofxKalmanFilter::hasLabel(unsigned int label) {
+	for (int i=0; i<aliveLabels.size(); i++) {
+		if (aliveLabels[i] == label)
+			return true;
+	}
+	return false;
+}
+
+void ofxKalmanFilter::predict(float dt) {
+	for (int i=0; i<aliveLabels.size(); i++) {
+		if (aliveLabels[i] != DEAD_LABEL) {
+			KalmanFilter::predict(dt, NULL, elementSize, i*elementSize);
+		}
+	}
 }
 
 void ofxKalmanFilter::correct(unsigned int label, float *element) {
@@ -39,7 +74,7 @@ void ofxKalmanFilter::correct(unsigned int label, float *element) {
         }
     }
     if (!found) {
-        for (int i=0; i<30; i++) {
+        for (int i=0; i<aliveLabels.size(); i++) {
             if (aliveLabels[i] == DEAD_LABEL) {
                 aliveLabels[i] = label;
                 KalmanFilter::reset(elementSize, i*elementSize);
@@ -57,5 +92,4 @@ void ofxKalmanFilter::get(unsigned int label, float *element) {
             break;
         }
     }
-
 }
--- src/ofxKalmanFilter.h
+++ src/ofxKalmanFilter.h
@@ -14,10 +14,16 @@ class ofxKalmanFilter : public KalmanFilter {
 public:
 
     // Initialize elements. Vec2f: size = 2, Vec3f: size = 3
-    void init(size_t elementSize, size_t count);
+    void init(unsigned int elementSize, unsigned int count);
 
     // Keep only these labels. Delete all other.
     void keepLabels(const std::vector<unsigned int> & labels);
+	unsigned int getNumAlive();
+	unsigned int getAliveLabel(unsigned int index);
+	bool hasLabel(unsigned int label);
+
+	// Predict alive models
+	void predict(float dt);
 
     // Correct an element
     void correct(unsigned int label, float * element);
@@ -26,6 +32,22 @@ public:
     void get(unsigned int label, float * element);
 
 protected:
-    size_t elementSize;
+	unsigned int elementSize;
     std::vector<unsigned int> aliveLabels;
+	unsigned int numAlive;
+};
+
+template <typename T>
+class ofxKalmanFilterT : public ofxKalmanFilter {
+public:
+	void init(size_t count) {ofxKalmanFilter::init(elementSize(), count);}
+	void correct(unsigned int label, T & element) {ofxKalmanFilter::correct(label, (float*)&element);}
+	T get(unsigned int label) {
+		T element;
+		ofxKalmanFilter::get(label, (float*)&element);
+		return element;
+	}
+
+protected:
+	unsigned int elementSize() const {return sizeof(T)/sizeof(float);}
 };
